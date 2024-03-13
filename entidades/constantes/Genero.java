0ad4ed95ac18ab52f1bddfaca1d4a18fe8f5192d
Schuab <kevinschuab@gmail.com>
2024-03-13 09:21:55 -0300

    refatoração da classe Genero.java

--- entidades/constantes/Genero.java
+++ entidades/constantes/Genero.java
@@ -2,29 +2,32 @@ package entidades.constantes;
 
 /**
  * Gêneros dos livros vendidos.
+ * Cada gênero tem um fator que influencia no cálculo do frete.
  * 
  * @author schuab
  */
-
 public enum Genero {
 
-    DRAMA(15),
-
-    SUSPENSE(10),
-
-    ROMANCE(5);
+    DRAMA(0.15), // O fator é 15%
+    SUSPENSE(0.10), // O fator é 10%
+    ROMANCE(0.05); // O fator é 5%
 
     private double fator;
 
     /**
      * Construtor.
      * 
-     * @param fator Valor por tipo que influencia no cálculo do frete.
+     * @param fator Valor por tipo que influencia no cálculo do frete (em %).
      */
     Genero(double fator) {
-        this.fator = fator / 100;
+        this.fator = fator;
     }
 
+    /**
+     * Obtém o fator que influencia no cálculo do frete.
+     * 
+     * @return O fator de influência no cálculo do frete (em %).
+     */
     public double getFator() {
         return fator;
     }
