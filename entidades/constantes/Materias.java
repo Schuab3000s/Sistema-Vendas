9fe949ce72fc234020f1310d9a1b66babb532c54
Schuab <kevinschuab@gmail.com>
2024-03-13 09:23:18 -0300

    refatoração da classe Meterias.java

--- entidades/constantes/Materias.java
+++ entidades/constantes/Materias.java
@@ -2,29 +2,34 @@ package entidades.constantes;
 
 /**
  * Quantidade de matérias do caderno vendido.
+ * Cada quantidade de matérias tem um fator que influencia no cálculo do frete.
  * 
  * @author schuab
  */
 public enum Materias {
 
-    M2(2),
-
-    M5(5),
-
-    M10(10);
+    M_2(0.2), // O fator é 0.2
+    M_5(0.5), // O fator é 0.5
+    M_10(1.0); // O fator é 1.0
 
     private double fator;
 
     /**
      * Construtor.
      * 
-     * @param fator Valor por tipo que influencia no cálculo do frete.
+     * @param fator Valor por tipo que influencia no cálculo do frete (em escala de 0 a 1).
      */
     Materias(double fator) {
-        this.fator = fator / 10;
+        this.fator = fator;
     }
 
+    /**
+     * Obtém o fator que influencia no cálculo do frete.
+     * 
+     * @return O fator de influência no cálculo do frete (em escala de 0 a 1).
+     */
     public double getFator() {
         return fator;
     }
 }
+
